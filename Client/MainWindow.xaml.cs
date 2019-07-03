a0eebd18fa495f420f6b98d557452f7f4c18b596
cougar91 <alpha.z91@mail.ru>
2019-07-03 14:16:24 +0600

    Добавьте файлы проекта.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string ipServer;
+        private int port;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+                dialog.InitialDirectory = Environment.SpecialFolder.Personal.ToString();
+                dialog.IsFolderPicker = true;
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    pathTextBox.Text = dialog.FileName;
+                }
+            }
+            catch (InvalidOperationException exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            ipServer = ipTextBox.Text;
+            port = int.Parse(portTextBox.Text);
+
+            TimerCallback tm = new TimerCallback(GetScreenFromServer);
+
+            int period = 0;
+            if (int.TryParse(periodTextBox.Text, out period) && period > 0)
+            {
+                Timer timer = new Timer(tm, client, 0, period * 1000);
+            }
+            else
+                MessageBox.Show("Проверьте правильность введенных данных", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void GetScreenFromServer(object state)
+        {
+            Socket client = state as Socket;
+            try
+            {
+                EndPoint srvEP = null;
+                string path = "";
+                Dispatcher.Invoke(() =>
+                {
+                    srvEP = new IPEndPoint(IPAddress.Parse(ipServer), port);
+
+                    string FileName = "desktop_image_" + (new Random().Next(int.MaxValue) + ".jpeg");
+                    path = pathTextBox.Text + "\\" + FileName;
+                });
+
+                string str = "Скриншот";
+                client.SendTo(Encoding.UTF8.GetBytes(str), srvEP);
+
+                byte[] buffer = new byte[1500];
+
+                // получить количество посылок
+                client.ReceiveFrom(buffer, ref srvEP);
+                int cntRecive = BitConverter.ToInt32(buffer, 0);
+
+                using (var stream = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    for (int i = 0; i < cntRecive; i++)
+                    {
+                        int recSize = client.ReceiveFrom(buffer, ref srvEP);
+                        if (recSize > 0)
+                        {
+                            stream.Write(buffer, 0, recSize);
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 12345));
+            WaitForRequest(serverSocket);
+        }
+        static void WaitForRequest(Socket serverSocket)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                EndPoint clientEndPoint = new IPEndPoint(0, 0);
+
+                int recSize = serverSocket.ReceiveFrom(buffer, ref clientEndPoint);
+                string keyWord = "Скриншот";
+
+                string path = "desktop_image_" + (new Random().Next(int.MaxValue) + ".jpeg"); ;
+                Bitmap bmp = ImageFromScreen();
+                bmp.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                if (keyWord == Encoding.UTF8.GetString(buffer, 0, recSize))
+                {
+                    using (var stream = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        int packetSize = 1500;
+                        long cntRecive = stream.Length / packetSize;
+                        long remainder = stream.Length % packetSize;
+
+                        if (remainder > 0) cntRecive++;
+                        serverSocket.SendTo(BitConverter.GetBytes((int)cntRecive), clientEndPoint);
+
+                        byte[] bmpBuffer = new byte[packetSize];
+
+                        for (int i = 0; i < (remainder > 0 ? cntRecive - 1 : cntRecive); i++)
+                        {
+                            stream.Read(bmpBuffer, 0, packetSize);
+                            serverSocket.SendTo(bmpBuffer, clientEndPoint);
+                            Thread.Sleep(20);
+                        }
+
+                        if (remainder > 0)
+                        {
+                            stream.Read(bmpBuffer, 0, (int)remainder);
+                            serverSocket.SendTo(bmpBuffer, 0, (int)remainder, SocketFlags.None, clientEndPoint);
+                        }
+                    }
+                }
+                WaitForRequest(serverSocket);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static Bitmap ImageFromScreen()
+        {
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height,
+                PixelFormat.Format32bppRgb);
+            using (Graphics graphics = Graphics.FromImage(bmp))
+            {
+                graphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y,
+                    0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            }
+            return bmp;
+        }
+    }
+}
